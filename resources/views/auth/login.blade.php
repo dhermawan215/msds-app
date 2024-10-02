69791475ffcfcc0a398c32c542d7a399459ee080
dicky hermawan <dhermawan215@gmail.com>
2024-10-02 16:53:08 +0700

    fix: redesign ui login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login - R&D Apps</title>
+    <title>Login - Lab System App</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
@@ -19,45 +19,83 @@
     <link rel="stylesheet" href="{{ asset('frontend/plugins/toastr/toastr.css') }}">
 </head>
 
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <h4><b>Login-Lab Service System </b></h4>
-            </div>
-            <div class="card-body">
-                <form action="javascript:;" method="post" id="loginForm">
-                    @csrf
-                    <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email" name="email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
+<body class="hold-transition">
+    <section class="bg-white py-3 py-md-5 py-xl-8">
+        <div class="container">
+            <div class="row gy-4 align-items-center">
+                <div class="col-12 col-md-6 col-xl-7">
+                    <div class="d-flex justify-content-center text-bg-primary">
+                        <div class="col-12 col-xl-9">
+                            <img class="img-fluid rounded mb-4" loading="lazy"
+                                src="{{ asset('assets/logo/zekindo-logo.png') }}" width="245" height="80"
+                                alt="BootstrapBrain Logo">
+                            <hr class="border-primary-subtle mb-4">
+                            <h2 class="h1 mb-4">Digsys - Lab System App</h2>
+                            <p class="lead mb-5">The lab system app is a digital system to help you create <b><span
+                                        class="text-primary">Sample
+                                        Requests, </span> Internal Material Usage Requests </b>& MSDS(coming soon).</p>
+                            <div class="text-endx">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"
+                                    fill="currentColor" class="bi bi-grip-horizontal" viewBox="0 0 16 16">
+                                    <path
+                                        d="M2 8a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                </svg>
                             </div>
+                            <p><b>&copy; 2024 - Zekindo Chemicals</b> <br>Powered by: IT Zekindo Companies</p>
                         </div>
                     </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password" name="password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
+                </div>
+                <div class="col-12 col-md-6 col-xl-5">
+                    <div class="card bg-primary border-0 rounded-5">
+                        <div class="card-body p-3 p-md-4 p-xl-5">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="mb-4">
+                                        <h3>Hello Again!</h3>
+                                        <p>Welcome back</p>
+                                    </div>
+                                </div>
                             </div>
+                            <form action="javascript:;" method="post" id="loginForm">
+                                @csrf
+                                <div class="input-group mb-3">
+                                    <input type="email" class="form-control" placeholder="Email" name="email">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-envelope"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input type="password" class="form-control" placeholder="Password" name="password">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-lock"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <!-- /.col -->
+                                    <div class="col-12">
+                                        <button type="submit" class="btn btn-light btn-block">Sign In</button>
+                                    </div>
+                                    <!-- /.col -->
+                                </div>
+                            </form>
+                            {{-- <div class="row">
+                                <div class="col-12">
+                                    <div
+                                        class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-end mt-4">
+                                        <a href="#">Forgot password</a>
+                                    </div>
+                                </div>
+                            </div> --}}
                         </div>
                     </div>
-                    <div class="row">
-                        <!-- /.col -->
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
+                </div>
             </div>
-            <!-- /.card-body -->
         </div>
-        <!-- /.card -->
-    </div>
+    </section>
     <!-- /.login-box -->
 
     <!-- jQuery -->
